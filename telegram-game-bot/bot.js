88ef4c5e6ceda88faed1452f482c2af8e9d6cc70
limicGGGamer <limiclee@gggamer.org>
2024-06-15 16:26:31 +0800

    updated

--- telegram-game-bot/bot.js
+++ telegram-game-bot/bot.js
@@ -1,9 +1,18 @@
 const { Telegraf } = require('telegraf');
+const crypto = require('crypto');
 const bot = new Telegraf('6990634330:AAEsfuDrFrTjCIlrM88P0CMiBGBwg3XkvkY');
 
 bot.start((ctx) => {
     const user = ctx.from;
-    const gameUrl = `https://limicgggamer.github.io/testgame.html?userId=${user.id}&firstName=${user.first_name}&lastName=${user.last_name}&username=${user.username}`;
+    const sessionId = crypto.randomBytes(16).toString('hex');
+    sessions[sessionId] = {
+        id: user.id,
+        firstName: user.first_name,
+        lastName: user.last_name,
+        username: user.username
+    };
+    const timestamp = Date.now(); // Use timestamp as a cache buster
+    const gameUrl = `https://limicgggamer.github.io/testgame.html?sessionId=${sessionId}&v=${timestamp}`;
     
     ctx.reply('Welcome! Click "Start Game" to begin playing.', {
         reply_markup: {
@@ -14,15 +23,6 @@ bot.start((ctx) => {
             ]
         }
     });
-    // ctx.reply('Welcome! Click "Start Game" to begin playing.', {
-    //     reply_markup: {
-    //         inline_keyboard: [
-    //             [
-    //                 { text: 'Start Game', web_app: { url: 'https://limicgggamer.github.io/testgame.html' } }
-    //             ]
-    //         ]
-    //     }
-    // });
 });
 
 bot.launch();
