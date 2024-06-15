2339c1a9a82317d3ccb3724bcb0d776178fd9e69
limicGGGamer <limiclee@gggamer.org>
2024-06-15 16:36:15 +0800

    updated

--- telegram-game-bot/bot.js
+++ telegram-game-bot/bot.js
@@ -2,19 +2,11 @@ const { Telegraf } = require('telegraf');
 const crypto = require('crypto');
 const bot = new Telegraf('6990634330:AAEsfuDrFrTjCIlrM88P0CMiBGBwg3XkvkY');
 
-const sessions = {};
-
 bot.start((ctx) => {
     const user = ctx.from;
     const sessionId = crypto.randomBytes(16).toString('hex');
-    sessions[sessionId] = {
-        id: user.id,
-        firstName: user.first_name,
-        lastName: user.last_name,
-        username: user.username
-    };
     const timestamp = Date.now(); // Use timestamp as a cache buster
-    const gameUrl = `https://limicgggamer.github.io/testgame.html?sessionId=${sessionId}&v=${timestamp}`;
+    const gameUrl = `https://limicgggamer.github.io/testgame.html?sessionId=${sessionId}&userId=${user.id}&firstName=${user.first_name}&lastName=${user.last_name}&username=${user.username}&v=${timestamp}`;
     
     ctx.reply('Welcome! Click "Start Game" to begin playing.', {
         reply_markup: {
